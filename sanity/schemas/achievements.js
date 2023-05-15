e22b6255e8ed4fe20c0c978df71d77c9e97ed7a4
ryanvanle <ryanle4321@gmail.com>
2023-05-15 04:05:34 -0700

    bridge sanity

---
+++ sanity/schemas/achievements.js
@@ -0,0 +1,38 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'achievements',
+  title: 'Bridge Achievements',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'award',
+      title: 'Award (formatting: Award, Location, Year)',
+      type: 'string',
+      validation: Rule => [
+        Rule.required(),
+      ],
+    }),
+    defineField({
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+      validation: Rule => [
+        Rule.required(),
+      ],
+    }),
+    defineField({
+      name: 'year',
+      title: 'Year',
+      type: 'string',
+      validation: Rule => [
+        Rule.required(),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'award',
+    },
+  },
+})
--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -2,5 +2,6 @@ import member from './member'
 import sponsor from './sponsor'
 import individual from './individual'
 import gallery from './gallery';
+import achievements from './achievements';
 
-export const schemaTypes = [member, sponsor, individual, gallery];
+export const schemaTypes = [member, sponsor, individual, gallery, achievements];
