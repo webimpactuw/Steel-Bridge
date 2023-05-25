c59108b41071223557ebfd6df028c7f376bbe8cc
ryanvanle <ryanle4321@gmail.com>
2023-05-25 16:23:15 -0700

    eoy build, mostly completely done but not yet :(

--- calendar.js
+++ calendar.js
@@ -52,32 +52,29 @@ import imageUrlBuilder from 'https://esm.sh/@sanity/image-url'
 
   function generateCalender(calendarLink) {
 
-    // id("calendar").innerHTML = `
-    // <iframe id="open-web-calendar"
-    // style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
-    // src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Figryanle4321%2540gmail.com%2Fpublic%2Fbasic.ics&amp;css=.dhx_scale_holder_now%2C%20.dhx_now%20.dhx_month_head%2C%20.dhx_now%20.dhx_month_body%20%7B%20background-color%3A%20%234b2e83%3B%7D%0A.event%20%7B%0Acolor%3A%20white%3B%0A%7D"
-    // sandbox="allow-scripts allow-same-origin allow-top-navigation"
-    // allowTransparency="true" scrolling="no"
-    // frameborder="0" height="600px" width="100%"></iframe>
-    // `
+    id("calendar").innerHTML = `
+    <iframe id="open-web-calendar"
+    style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
+    src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Foutlook.office365.com%2Fowa%2Fcalendar%2Fd45ad64e0ac441d6a6d09a6ae5e12957%40uw.edu%2F833dfe655e6442f7bb12c1f8da2aa877556960875527581770%2Fcalendar.ics&amp;css=.dhx_scale_holder_now%2C%20.dhx_now%20.dhx_month_head%2C%20.dhx_now%20.dhx_month_body%20%7B%20background-color%3A%20%234b2e83%3B%7D%0A.event%20%7B%0Acolor%3A%20white%3B%0A%7D"
+    sandbox="allow-scripts allow-same-origin allow-top-navigation"
+    allowTransparency="true" scrolling="no"
+    frameborder="0" height="600px" width="100%"></iframe>
+    `
 
     //https://outlook.office365.com/owa/calendar/d45ad64e0ac441d6a6d09a6ae5e12957@uw.edu/833dfe655e6442f7bb12c1f8da2aa877556960875527581770/calendar.ics
 
-    console.log(`https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Figryanle4321%2540gmail.com%2Fpublic%2Fbasic.ics&amp;css=.dhx_scale_holder_now%2C%20.dhx_now%20.dhx_month_head%2C%20.dhx_now%20.dhx_month_body%20%7B%20background-color%3A%20%234b2e83%3B%7D%0A.event%20%7B%0Acolor%3A%20white%3B%0A%7D`);
-    encodeURI()
 
-    console.log(calendarLink);
-    let calendarIFrame = gen('iframe');
-    calendarIFrame.style = "border: 0; background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;";
-    calendarIFrame.src = "https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=" + encodeURIComponent(calendarLink) + "&amp;css=.event%2C%20.dhx_cal_tab.active%2C%20.dhx_cal_tab.active%3Ahover%20%7Bbackground-color%3A%20%234b2e83%3B%7D%20.dhx_month_head%2C%20.dhx_cal_tab%2C%20.dhx_cal_today_button%20%7Bcolor%3A%20%234b2e83%3B%7D%20.dhx_cal_tab%2C%20.dhx_cal_tab.active%20%7Bborder-color%3A%20%234b2e83%3B%7D%0A"
-    calendarIFrame.sandbox = "allow-scripts allow-same-origin allow-top-navigation";
-    calendarIFrame.allowTransparency = "true";
-    calendarIFrame.scrolling = "no";
-    calendarIFrame.frameborder = "0";
-    calendarIFrame.height = "600px";
-    calendarIFrame.width = "100%"
+    // let calendarIFrame = gen('iframe');
+    // calendarIFrame.style = "border: 0; background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;";
+    // calendarIFrame.src = "https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=" + encodeURIComponent(calendarLink) + "&amp;css=.event%2C%20.dhx_cal_tab.active%2C%20.dhx_cal_tab.active%3Ahover%20%7Bbackground-color%3A%20%234b2e83%3B%7D%20.dhx_month_head%2C%20.dhx_cal_tab%2C%20.dhx_cal_today_button%20%7Bcolor%3A%20%234b2e83%3B%7D%20.dhx_cal_tab%2C%20.dhx_cal_tab.active%20%7Bborder-color%3A%20%234b2e83%3B%7D%0A"
+    // calendarIFrame.sandbox = "allow-scripts allow-same-origin allow-top-navigation";
+    // calendarIFrame.allowTransparency = "true";
+    // calendarIFrame.scrolling = "no";
+    // calendarIFrame.frameborder = "0";
+    // calendarIFrame.height = "600px";
+    // calendarIFrame.width = "100%"
 
-    id("calendar").append(calendarIFrame);
+    // id("calendar").append(calendarIFrame);
   }
 
   // from open web calendar,
