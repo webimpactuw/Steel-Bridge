0cebf6da115b89a63da15b949d14cd896994c73b
blloop <42186044+blloop@users.noreply.github.com>
2025-01-04 23:34:26 -0800

    compile studio assets

---
+++ studio/static/index-52c6e3e3-7877ce65.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-b1fbe7cd-6c2cd0d4.js";import"./sanity-89bdfa9d.js";import"./index-aca3d004.js";export{m as DocumentPaneProvider,a as default};
