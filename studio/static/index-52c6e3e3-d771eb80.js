ed094cb0c81ffa8e5a662b38609ac092e9d8f1b9
blloop <42186044+blloop@users.noreply.github.com>
2025-01-06 18:54:04 -0800

    remove unused studio

--- studio/static/index-52c6e3e3-d771eb80.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-b1fbe7cd-68acde3e.js";import"./sanity-5248c0f2.js";import"./index-714849b1.js";export{m as DocumentPaneProvider,a as default};
