e94cdf24639fa83fe54cafa03f8f3692807438d0
blloop <42186044+blloop@users.noreply.github.com>
2025-02-04 17:34:20 -0800

    remove calendar

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -3,8 +3,7 @@ import sponsor from './sponsor'
 import individual from './individual'
 import gallery from './gallery';
 import achievements from './achievements';
-import calendar from './calendar';
 import joinUs from './join-us';
 import groupPicture from './group-picture';
 
-export const schemaTypes = [member, groupPicture, sponsor, individual, gallery, achievements, calendar, joinUs];
+export const schemaTypes = [member, groupPicture, sponsor, individual, gallery, achievements, joinUs];
